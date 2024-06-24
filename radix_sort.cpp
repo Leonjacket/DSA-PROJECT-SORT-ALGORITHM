9e0f0194ac82aaed91d886de3c6f34e7aa3a5d93
mankie3925 <dhdmanh23@clc.fitus.edu.vn>
2024-06-24 21:48:01 +0700

    update_cmp

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -44,6 +44,37 @@ void radix_sort(int a[], int n)
     }
 }
 
+void count_sort_cmp(int a[], int n, int exp, int &cmp)
+{
+    int output[n];
+    int i, count[10] = {0};
+
+    for (i = 0; i < n; i++)
+    {
+        cmp++;
+        count[(a[i] / exp) % 10]++;
+    }
+
+    for (i = 1; i < 10; i++)
+    {
+        cmp++;
+        count[i] += count[i - 1];
+    }
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        cmp++;
+        output[count[(a[i] / exp) % 10] - 1] = a[i];
+        count[(a[i] / exp) % 10]--;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        cmp++;
+        a[i] = output[i];
+    }
+}
+
 void radix_sort_cmp(int a[], int n, int &cmp)
 {
     cmp = 0;
@@ -51,16 +82,17 @@ void radix_sort_cmp(int a[], int n, int &cmp)
 
     for (int i = 1; i < n; i++)
     {
-        if (a[i] > max)
+        cmp++;
+        if (a[i] > max && cmp++)
         {
             max = a[i];
         }
     }
-    // comparison of this get-max function will not be included with radix_sort function.
 
     for (int exp = 1; max / exp > 0; exp *= 10)
     {
-        count_sort(a, n, exp);
+        cmp++;
+        count_sort_cmp(a, n, exp, cmp);
     }
 }
 
