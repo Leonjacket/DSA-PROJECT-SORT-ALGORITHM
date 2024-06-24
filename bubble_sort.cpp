6ea6ddd991242f92e46dd5df83a972d5a0ca41fb
ng0x <161100173+ng0x@users.noreply.github.com>
2024-06-24 19:17:23 +0700

    bubble_sort: xong cơ bản (chưa biết có bug ở hàm đếm không)

---
+++ bubble_sort.cpp
@@ -0,0 +1,53 @@
+#include "ngoc.h"
+void bubble_sort(int *&a, int size)
+{
+	bool changed;
+	for(int i = 0; i < size - 1; ++i)
+	{
+		changed = false;
+		for(int j = 0; j < size - i - 1; ++j)
+		{
+			if(a[j] > a[j + 1])
+			{
+				swap(a[j], a[j + 1]);
+				changed = true;
+			}
+		}
+		if(changed == false)
+		{
+			break;
+		}
+	}
+}
+void bubble_sort_cmp(int *&a, int size, int &cmp)
+{
+    bool changed;
+    cmp = 0;
+	for(int i = 0; i < size; ++i)
+	{ 
+		++cmp;
+		changed = false;
+		for(int j = 0;j < size - i - 1; ++j)
+		{
+            ++cmp;
+			if(++cmp && (a[j] > a[j + 1]))
+			{
+				swap(a[j], a[j + 1]);
+				changed = true;
+			}
+		}
+		++cmp;
+		if(++cmp && changed == false) 
+		{
+			break;
+		}
+	}
+	++cmp;
+}
+void bubble_sort_time(int *&a, int size, double &time)
+{
+	auto start = chrono::high_resolution_clock::now();
+    bubble_sort(a, size);
+    auto end = chrono::high_resolution_clock::now();
+    dur = chrono::duration<double, milli>(end - start).count();
+}
