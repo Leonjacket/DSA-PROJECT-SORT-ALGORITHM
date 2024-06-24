6ec532a9350b1af5bce7edbd4f2bfc99d36839be
Bachiboyy <thbach23@clc.fitus.edu.vn>
2024-06-24 10:58:32 +0700

    Update bach.h

--- bach.h
+++ bach.h
@@ -10,3 +10,13 @@ typedef __darwin_clock_t clock_t;
 #endif /* _CLOCK_T */
 typedef unsigned long __darwin_clock_t; /* clock() */
 #define CLOCKS_PER_SEC  1000000    /* [XSI] */
+
+void merge(int* l, int* r, int* a, int size);
+
+void merge_sort(int* a, int size);
+
+void merge_cmp(int* l, int* r, int* a, int size, int& cmp);
+
+void merge_sort_cmp(int*& a, int size, int& cmp);
+
+void merge_sort_time(int*& a, int size, int& dur);
