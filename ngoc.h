bc283ee1f5324f172cd4fd4e021aefc3396d5bc4
ng0x <161100173+ng0x@users.noreply.github.com>
2024-06-24 11:14:23 +0700

    quick sort: tạo prototype và file cpp cho quick_sort

--- ngoc.h
+++ ngoc.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <time.h>
 #include "DataGenerator.cpp"
 using namespace std;
 void command_line_1(int argc,char* argv[]);
@@ -9,3 +10,6 @@ void command_line_3(int argc,char* argv[]);
 void command_line_4(int argc,char* argv[]);
 void command_line_5(int argc,char* argv[]);
 bool have_extension(string filename);
+void quick_sort(int *&a, int size);
+void quick_sort_cmp(int *&a, int size, int &cmp);
+double quick_sort_time(int *&a, int size, int &dur);
