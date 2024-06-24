ed824c65d8e535a4290bb6c26b038db2615203aa
ng0x <161100173+ng0x@users.noreply.github.com>
2024-06-24 15:32:39 +0700

    bubble_sort: thêm header vô từ branch quick_sort

---
+++ ngoc.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <algorithm>
+#include "DataGenerator.cpp"
+using namespace std;
+void command_line_1(int argc,char* argv[]);
+void command_line_2(int argc,char* argv[]);
+void command_line_3(int argc,char* argv[]);
+void command_line_4(int argc,char* argv[]);
+void command_line_5(int argc,char* argv[]);
+bool have_extension(string filename);
+int partition(int* &a, int l, int r, int pivot); //hàm phụ cho quick sort
+void quick_sort_algo(int *&a, int s, int e); //hàm phụ cho quick sort
+void quick_sort(int *&a, int size); // tham khảo ở  https://youtu.be/pM-6r5xsNEY?si=U1rUfohX2kWGmz4M
+void quick_sort_cmp(int *&a, int size, int &cmp); //hàm phụ cho quick sort cmp
+void partition_cmp(int *&a, int l, int r, int e, int &cmp); //hàm phụ cho quick sort cmp
+void bubble_sort(int *&a, int size);
+void bubble_sort_cmp(int *&a, int size, int &cmp); 
+void bubble_sort_time(int *&a, int size, double &time);
