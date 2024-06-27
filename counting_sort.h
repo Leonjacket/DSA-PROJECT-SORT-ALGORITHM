1acf74e16afa29afa186f1a20effd01459af2336
Bachiboyy <thbach23@clc.fitus.edu.vn>
2024-06-27 21:51:56 +0700

    1

--- counting_sort.h
+++ counting_sort.h
@@ -9,4 +9,4 @@ void counting_sort(int*& arr, int n);
 
 void counting_sort_cmp(int*& arr, int n, int& cmp);
 
-void flash_sort_time(int*& a, int n, double& dur);
+void counting_sort_time(int*& a, int n, double& dur);
