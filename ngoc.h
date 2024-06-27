f80c1d2997e003d4ca6ec7f10d952bf24298971e
ng0x <161100173+ng0x@users.noreply.github.com>
2024-06-27 20:35:53 +0700

    bubble_sort: sửa gộp chung file cpp vô header luôn

--- ngoc.h
+++ ngoc.h
@@ -1,4 +1,4 @@
-#pragma once
+
 #include <iostream>
 #include <string>
 #include <chrono>
@@ -19,3 +19,56 @@ void partition_cmp(int *&a, int l, int r, int e, int &cmp); //hàm phụ cho qui
 void bubble_sort(int *&a, int size);
 void bubble_sort_cmp(int *&a, int size, int &cmp); 
 void bubble_sort_time(int *&a, int size, double &dur);
+
+void bubble_sort(int *&a, int size)
+{
+	bool changed;
+	for(int i = 0; i < size - 1; ++i)
+	{
+		changed = false;
+		for(int j = 0; j < size - i - 1; ++j)
+		{
+			if(a[j] > a[j + 1])
+			{
+				swap(a[j], a[j + 1]);
+				changed = true;
+			}
+		}
+		if(changed == false)
+		{
+			break;
+		}
+	}
+}
+void bubble_sort_cmp(int *&a, int size, int &cmp)
+{
+    bool changed;
+    cmp = 0;
+	for(int i = 0; i < size; ++i)
+	{ 
+		++cmp;
+		changed = false;
+		for(int j = 0;j < size - i - 1; ++j)
+		{
+            ++cmp;
+			if(++cmp && (a[j] > a[j + 1]))
+			{
+				swap(a[j], a[j + 1]);
+				changed = true;
+			}
+		}
+		++cmp;
+		if(++cmp && changed == false) 
+		{
+			break;
+		}
+	}
+	++cmp;
+}
+void bubble_sort_time(int *&a, int size, double &dur)
+{
+	auto start = chrono::high_resolution_clock::now();
+    bubble_sort(a, size);
+    auto end = chrono::high_resolution_clock::now();
+    dur = chrono::duration<double, milli>(end - start).count();
+}
