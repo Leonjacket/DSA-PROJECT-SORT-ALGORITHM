be05de58419c54678dbc8343889119dc3d9165ef
ng0x <161100173+ng0x@users.noreply.github.com>
2024-06-29 01:07:35 +0700

    viết các lệnh xử lý cái command (chưa update từ file thử qua)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include "sources/DataGenerator.cpp"
 #include ".\methods\all.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "test";
+    string tmp = argv[1];
+    string filename = argv[2];
+    if(tmp == "-a" && have_extension(filename) && argc == 5)
+    {
+        command_line_1(argc, argv);
+    }
+    else if(argv[1] == "-a" && argc == 6)
+    {
+        command_line_2(argc, argv);
+    }
+    else if(argv[1] == "-a" && have_extension(filename) == false && argc == 5)
+    {
+        command_line_3(argc, argv);
+    }
+    else if(argv[1] == "-c" && argc == 5)
+    {
+        command_line_4(argc, argv);
+    }
+    else if(argv[1] == "-c" && argc == 6)
+    {
+        command_line_5(argc, argv);
+    }
     return 0;
 }
