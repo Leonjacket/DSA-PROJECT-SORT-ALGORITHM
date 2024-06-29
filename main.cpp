b20c76e85db35b926b4f581c1f220d9606d830f7
ng0x <161100173+ng0x@users.noreply.github.com>
2024-06-29 16:36:12 +0700

    sửa xong command line và hàm main

--- main.cpp
+++ main.cpp
@@ -1,29 +1,30 @@
 #include "sources/DataGenerator.cpp"
 #include ".\methods\all.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     string tmp = argv[1];
     string filename = argv[2];
-    cout << (argv[1]);
-    if (tmp == "-a" && have_extension(filename) && argc == 5)
+    string arg1 = argv[2];
+    cout << "Argument 2 received: " << arg1 << endl;
+    if(tmp == "-a" && have_extension(filename) && argc == 5)
     {
         command_line_1(argc, argv);
     }
-    else if (argv[1] == "-a" && argc == 6)
+    else if(tmp == "-a" && argc == 6)
     {
-        cout << "cmd 2 called";
         command_line_2(argc, argv);
     }
-    else if (argv[1] == "-a" && have_extension(filename) == false && argc == 5)
+    else if(tmp == "-a" && have_extension(filename) == false && argc == 5)
     {
+        cout << 3 << endl;
         command_line_3(argc, argv);
     }
-    else if (argv[1] == "-c" && argc == 5)
+    else if(tmp == "-c" && argc == 5)
     {
         command_line_4(argc, argv);
     }
-    else if (argv[1] == "-c" && argc == 6)
+    else if(tmp == "-c" && argc == 6)
     {
         command_line_5(argc, argv);
     }
