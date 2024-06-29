6b153cc22adba412cebf0b9e9051f864c8299487
Leonjacket <145545700+Leonjacket@users.noreply.github.com>
2024-06-29 14:00:17 +0700

    read_file: update

--- read_file.h
+++ read_file.h
@@ -12,9 +12,10 @@ bool read_file(int*& a, int n, string file)
         cout << "Error! Can't open file\n";
         return false;
     }
-
     in >> n;
+    a = new int[n];
     for(int i = 0; i < n; i++)
         in >> a[i];
+    in.close();
     return true;
 }
